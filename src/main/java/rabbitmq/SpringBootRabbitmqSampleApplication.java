01498bc9e60bf8c3ef8bc469f0c8a0b54ef69366
sakuoden <4oden18@gmail.com>
2023-01-27 21:05:54 +0900

    first

---
+++ src/main/java/rabbitmq/SpringBootRabbitmqSampleApplication.java
@@ -0,0 +1,13 @@
+package rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitmqSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRabbitmqSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rabbitmq/SpringBootRabbitmqSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitmqSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
