45ef76507437596e048edf7e2e61824ae3141fb6
sakuoden <4oden18@gmail.com>
2023-01-30 18:22:44 +0900

    Bean定義されてしまっているのを修正する。

--- src/main/java/rabbitmq/tutorial/tutorial1/Tutorial1Receiver.java
+++ src/main/java/rabbitmq/tutorial/tutorial1/Tutorial1Receiver.java
@@ -2,9 +2,7 @@ package rabbitmq.tutorial.tutorial1;
 
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.stereotype.Component;
 
-@Component
 @RabbitListener(queues = "hello")
 public class Tutorial1Receiver {
     @RabbitHandler
